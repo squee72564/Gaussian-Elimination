7a06b43d4f58d2a2656dfd2ca3961aa65145b927
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-02-15 16:35:20 -0800

    Added .h file for Gaussian Elimination function

---
+++ GaussElim.h
@@ -0,0 +1,13 @@
+#ifndef GUASS_ELIM
+#define GUASS_ELIM
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+
+void printMat(const std::vector<std::vector<float>> &mat);
+void gaussianElim(const int n, std::vector<std::vector<float>>& mat);
+
+
+#endif
