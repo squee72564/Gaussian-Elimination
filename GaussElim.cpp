7c4ffd05acf9622da2a08d8277538d59af87d707
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-02-15 16:36:56 -0800

    Started Gaussian Elimination fucntion.
    Needs troubleshooting; index and size vector are sucessfully initailized but errors are happening within main for loop for Gauss procedure code past that

---
+++ GaussElim.cpp
@@ -0,0 +1,62 @@
+#include "GaussElim.h"
+
+void printMat(const std::vector<std::vector<float>> &mat) {
+	int n = mat.size();
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			std::cout << mat[i][j] << "\t";
+		}
+		std::cout << "\n";
+	}
+}
+
+
+void gaussianElim(const int n, std::vector<std::vector<float>>& mat) {
+	std::vector<int> l(n, 0);
+	std::vector<float> s(n, 0);
+
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	float smax = 0;
+	float rmax = 0;
+	float xmult = 0;
+	float r = 0;
+
+	for (i = 0; i < n; i++) {
+		l[i] = i;
+		smax = 0;
+		for (j = 0; j < n; j++) {
+			smax = std::max(smax, mat[i][j]);
+		}
+		s[i] = float(smax);
+	}
+
+	std::cout << "Initialized index and max vector\n";
+
+	for (k = 0; k < n-1; k++ ) {
+		std::cout << "K: " << k << "\n";
+		rmax = 0;
+		for (i = k; i < n; i++) {
+			r = mat[l[i]][k] / s[l[i]];
+			if (r > rmax) {
+				rmax = r;
+				j = i;
+			}	
+		}
+		
+		int temp = l[i];
+		l[i] = l[k];
+		l[k] = temp;
+
+		for (i = k+1; i < n; i++) {
+			xmult = mat[l[i]][k] / mat[l[k]][k];
+			mat[l[i]][k] = xmult;
+			for (j = k+1; j < n; j++) {
+				mat[l[i]][j] = mat[l[i]][j]-xmult*mat[l[k]][j];
+			}
+		}
+	}
+
+	printMat(mat);
+}
