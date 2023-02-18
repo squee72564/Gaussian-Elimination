26a571681e150f0538165a8ed480ec56574637d5
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-02-17 19:05:28 -0800

    Changed function declarations
    guassianElim() now takes in the index vector by reference, so it can be used with the matrix for the solve function
    
    solve() added and takes in matrix, index vector, and b vector representing dependent variable values for the system of equations. This allows for multiple b vectors to be used for a single system of equations.

--- GaussElim.h
+++ GaussElim.h
@@ -6,8 +6,7 @@
 #include <iostream>
 
 
-void printMat(const std::vector<std::vector<float>> &mat);
-void gaussianElim(const int n, std::vector<std::vector<float>>& mat);
-
+void gaussianElim(const int n, std::vector<std::vector<float>>& mat, std::vector<int>& l);
+std::vector<float> solve(int n, const std::vector<std::vector<float>> &mat, const std::vector<int> &l, std::vector<float> &b);
 
 #endif
