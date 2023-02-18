12e9186e6565c4775b82afe139ec6021c392ae00
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-02-17 19:14:00 -0800

    Modified gaussElim function, added solve function, and moved printMat to main.
    Solve is used with after a system of equations has been processed by the gaussElim function. It takes in the index vector l by reference, as well as a vector b which is the dependent variables for each equation in the system. This allows for multiple b vectors to be used for one system of equations. Solve returns a vector of each x value from x_1 ... x_n.

--- GaussElim.cpp
+++ GaussElim.cpp
@@ -1,27 +1,15 @@
 #include "GaussElim.h"
 
-void printMat(const std::vector<std::vector<float>> &mat) {
-	int n = mat.size();
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			std::cout << mat[i][j] << "\t";
-		}
-		std::cout << "\n";
-	}
-}
-
-
-void gaussianElim(const int n, std::vector<std::vector<float>>& mat) {
-	std::vector<int> l(n, 0);
+void gaussianElim(const int n, std::vector<std::vector<float>>& mat, std::vector<int>& l) {
 	std::vector<float> s(n, 0);
 
 	int i = 0;
 	int j = 0;
 	int k = 0;
-	float smax = 0;
-	float rmax = 0;
-	float xmult = 0;
-	float r = 0;
+	float smax = 0.0f;
+	float rmax = 0.0f;
+	float xmult = 0.0f;
+	float r = 0.0f;
 
 	for (i = 0; i < n; i++) {
 		l[i] = i;
@@ -54,5 +42,33 @@ void gaussianElim(const int n, std::vector<std::vector<float>>& mat) {
 			}
 		}
 	}
-	printMat(mat);
+}
+
+
+std::vector<float> solve(int n, const std::vector<std::vector<float>> &mat, const std::vector<int> &l, std::vector<float> &b) {
+
+	std::vector<float> x(n, 0);
+	
+	int i = 0;
+	int k = 0;
+	int j = 0;
+	float sum = 0.0f;
+
+	for (k = 1; k < n-1; k++) {
+		for (i = k+1; i < n; i++ ) {
+			b[l[i]] = b[l[i]] - mat[l[i]][k]*b[l[k]];	
+		}
+	}
+	
+	x[n-1] = b[l[n-1]] / mat[l[n-1]][n-1];
+
+	for (i = n-1; i>=0; i--) {
+		sum = b[l[i]];
+		for (j = i+1; j < n; j++) {
+			sum = sum - mat[l[i]][j]*x[j];
+		}
+		x[i] = sum / mat[l[i]][i];
+	}
+
+	return x;
 }
