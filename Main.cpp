549c90d6c6b858e2fb23255cc5f58af6272f3022
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-02-17 19:15:42 -0800

    Added b and l vector. moved printMat function from GaussElim.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include "GaussElim.h"
 
+void printMat(const std::vector<std::vector<float>> &mat); 
+void printVec(const std::vector<float> &vec);
+
 int main (int argc, char **argv) {
-	std::vector<std::vector<float>> mat = {{1.0,2.0,3.0},{4.0,5.0,6.0},{7.0,8.0,9.0}};
+	std::vector<std::vector<float>> mat = { {3,-13,9,3},
+						{-6,4,1,-18},
+						{6,-2,2,4},
+       						{12,-8,6,10} };
+	std::vector<float> b = {-19, -34, 16, 26};
+	std::vector<int> l(b.size(), 0);
 	int n = mat[0].size();
-	gaussianElim(n, mat);	
+
+	gaussianElim(n, mat, l);	
+	printMat(mat);
+
+	std::vector<float> answerVector = solve(n, mat, l, b);
+	printVec(answerVector);
 }
+
+void printMat(const std::vector<std::vector<float>> &mat) {
+	int n = mat.size();
+	std::cout << "\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			std::cout << mat[i][j] << "\t";
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+}
+
+void printVec(const std::vector<float> &vec) {
+	std::cout << "\n";
+	for (int i = 0; i < vec.size(); i++) {
+		std::cout << vec[i] << "\t";
+	}
+	std::cout << "\n";
+}
+
